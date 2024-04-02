345b7afeffef2ba567ae8cdda4d86a62d3e64cb8
Bharat Chhabra <Bharat.chhabra@tevvo.com>
2024-04-02 22:52:12 +0530

    2 April 2024

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import CarModels from './car-models/cars'
 import Banks from "./bank-names/banks.jsx"
 import './index.css'
+import Login from './login/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CarModels />
+    <Login />
   </React.StrictMode>,
 );
